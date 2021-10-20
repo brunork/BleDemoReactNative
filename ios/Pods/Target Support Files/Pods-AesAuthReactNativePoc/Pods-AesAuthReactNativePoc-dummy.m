b54ef45d6aa175836e91685e1ece41ff40020a5a
Bruno Kocziceski <brunork@gmail.com>
2021-10-20 09:15:33 -0300

    F6 write implementation flow

---
+++ ios/Pods/Target Support Files/Pods-AesAuthReactNativePoc/Pods-AesAuthReactNativePoc-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AesAuthReactNativePoc : NSObject
+@end
+@implementation PodsDummy_Pods_AesAuthReactNativePoc
+@end
---
+++ ios/Pods/Target Support Files/Pods-AesAuthReactNativePocTests/Pods-AesAuthReactNativePocTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AesAuthReactNativePocTests : NSObject
+@end
+@implementation PodsDummy_Pods_AesAuthReactNativePocTests
+@end
